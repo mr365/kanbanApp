18f6cb8861e6d29d4ab01f2104a3cb6b85ca7b16
Mariprasad Ramakrishna <mr365@njit.edu>
2016-05-16 14:52:47 -0400

    stage set the app to take shape form here

---
+++ app/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import uuid from 'node-uuid';
+
+export default class App extends React.Component {
+  render() {
+	const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+  return (
+  	<div>
+  	  <ul>{ notes.map(note =>
+	  		<li key={note.id}>{note.task}</li>
+	  	 )}</ul>
+  	</div>
+  	);
+  }
+}
