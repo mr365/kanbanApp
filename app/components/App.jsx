ca0583577b38b46b377d47b63fa3699bfaced328
Mariprasad Ramakrishna <prasad@Mariprasads-MacBook-Pro.local>
2016-06-07 22:27:29 -0400

    implemented flux  using alt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import uuid from 'node-uuid';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions.js';
+import NoteStore from '../store/NoteStore.js';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-		  notes : [
-      		  {
-		        id: uuid.v4(),
-		        task: 'Learn Webpack'
-		      },
-		      {
-		        id: uuid.v4(),
-		        task: 'Learn React'
-		      },
-		      {
-		        id: uuid.v4(),
-		        task: 'Do laundry'
-		      }
-		    ]
-		};
+    this.state = NoteStore.getState();
 	}
+
+  componentDidMount(){
+    NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillMount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged = (state) => {
+    this.setState(state);
+  };
+
   render() {
   	const notes = this.state.notes;
   return (
@@ -31,34 +30,20 @@ export default class App extends React.Component {
   	</div>
   	);
   }
+
   addNote= () => {
-  	this.setState({
-  		notes: this.state.notes.concat([
-  		{
-  			id: uuid.v4(),
-  			task : 'maris got brains'
-  		}
-  			])
-  	});
-  };
+  	NoteActions.create({task: 'New task'});
+  }
+
   editNote = (id, task) => {
     // don't modify if trying to set an empty value
     if(!task.trim()) {
       return;
     }
+    NoteActions.update({id, task});
+  }
 
-    const notes = this.state.notes.map( note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-      return note;
-    });
-
-    this.setState({notes});
-  };
   deleteNote = (id) => {
-    this.setState({
-      notes : this.state.notes.filter(note => note.id !== id)
-    });
+    NoteActions.delete(id);
   }
 }
