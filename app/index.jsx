adb35b7ee1c6e240c08db52d975fe753f6b92efa
Mariprasad Ramakrishna <prasad@Mariprasads-MacBook-Pro.local>
2016-06-07 23:47:57 -0400

    complete working notes app first pass

--- app/index.jsx
+++ app/index.jsx
@@ -2,5 +2,10 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt.js';
+import storage from './libs/storage.js';
+import persist from './libs/persist.js';
+
+persist(alt, storage, 'app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
